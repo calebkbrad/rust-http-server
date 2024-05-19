04f6b9dfa9a8213c010c052ae476b55128477498
caleb <cbrad022@odu.edu>
2024-05-19 12:54:35 -0400

    get content method works for existing file

--- workspace/tools/src/lib.rs
+++ workspace/tools/src/lib.rs
@@ -1,5 +1,11 @@
-pub fn add(left: usize, right: usize) -> usize {
-    left + right
+use std::{
+    fs,
+    error::Error,
+    env,
+    path::Path
+};
+fn get_content_or_404(file_path: &str) -> Result<String, std::io::Error> {
+    fs::read_to_string(file_path)
 }
 
 #[cfg(test)]
@@ -7,8 +13,15 @@ mod tests {
     use super::*;
 
     #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
+    fn test_get_content() {
+        let root = Path::new("/home/caleb/rust-http-server/workspace");
+        assert!(env::set_current_dir(&root).is_ok());
+        let actual_content = "<h1>this is html!</h1>".as_bytes();
+        
+        let binding = get_content_or_404("test_files/content.html").expect("File was not found");
+        let content = binding.as_bytes();
+
+        assert_eq!(actual_content, content);
+        
     }
 }
